7020922db6a9f561cdcdbb415803a6cd8a7cf6b5
Matt Rosendin <matt.rosendin@gmail.com>
2017-09-25 01:09:35 -0700

    add a signup view

--- polyledger/account/urls.py
+++ polyledger/account/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
     url(r'^password_reset_done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     url(r'^password_reset_confirm/$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    url(r'^password_reset_complete/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
+    url(r'^password_reset_complete/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    url(r'^signup/$', views.signup, name='signup')
 ]
--- polyledger/account/views.py
+++ polyledger/account/views.py
@@ -2,25 +2,24 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
 
 
-# def login(request):
-#     username = request.POST['username']
-#     password = request.POST['password']
-#     user = authenticate(request, username=username, password=password)
-#     if user is not None:
-#         login(request, user)
-#         redirect('index')
-#     else:
-#         # Return an 'invalid login' error message.
-#         pass
-
-
-def logout(request):
-    logout(request)
-    return redirect('login')
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('account:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 @login_required
 def index(request):
