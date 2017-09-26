fe7bfc4658d824bbf384e72f8625a5aa86e0b14f
Matt Rosendin <matt.rosendin@gmail.com>
2017-09-26 14:11:56 -0700

    add site-wide static directory

--- polyledger/polyledger/settings.py
+++ polyledger/polyledger/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Set auth redirects and URLs
 # https://docs.djangoproject.com/en/1.11/ref/settings/#login-redirect-url
