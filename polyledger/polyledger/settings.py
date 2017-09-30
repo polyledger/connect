a61a43ae32728dba892da6a26bd76ce727e70412
Matt Rosendin <matt.rosendin@gmail.com>
2017-09-30 11:42:47 -0700

    ignore project-level static files

--- polyledger/polyledger/settings.py
+++ polyledger/polyledger/settings.py
@@ -120,11 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'static'))
 
 
 # Set auth redirects and URLs
