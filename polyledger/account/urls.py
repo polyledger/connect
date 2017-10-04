174547f71db38ef5590290711aaea7b944e449f0
Matt Rosendin <matt.rosendin@gmail.com>
2017-10-03 22:32:03 -0700

    [bug-fix] load from method

--- polyledger/account/urls.py
+++ polyledger/account/urls.py
@@ -7,11 +7,11 @@ app_name = 'account'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
-    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    url(r'^password_reset_done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    url(r'^password_reset_confirm/$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    url(r'^password_reset_complete/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset_done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^password_reset_confirm/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^password_reset_complete/$', auth_views.password_reset_complete, name='password_reset_complete'),
     url(r'^signup/$', views.signup, name='signup')
 ]
