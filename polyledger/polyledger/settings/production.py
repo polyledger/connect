dc2a09c97de0e81151287b2e563073409435fe08
Matt Rosendin <matt.rosendin@gmail.com>
2017-10-03 23:33:21 -0700

    set staticfiles for production

--- polyledger/polyledger/settings/production.py
+++ polyledger/polyledger/settings/production.py
@@ -113,7 +113,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../..', 'static'))
+STATIC_ROOT = '/app/static/'
+STATICFILES_DIRS = [
+    '/app/polyledger/static'
+]
 
 
 # Set auth redirects and URLs
