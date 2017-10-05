38136c4fc163fbe5f2a4e8afc5a9715805a03403
Matt Rosendin <matt.rosendin@gmail.com>
2017-10-04 19:24:55 -0700

    add local network IP to allowed hosts

--- polyledger/polyledger/settings/local.py
+++ polyledger/polyledger/settings/local.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '192.168.1.22']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
