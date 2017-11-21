e524f666ee5563b0dfe1a509929f19ee7e74eef6
Matt Rosendin <matt.rosendin@gmail.com>
2017-11-21 09:39:15 -0800

    [bug-fix] fix 'NoneType' object is not iterable TypeError at /admin/account/user/add

--- account/admin.py
+++ account/admin.py
@@ -66,24 +66,23 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_admin')
-    list_filter = ('is_admin'),
+    list_display = ('email', 'first_name', 'last_name', 'is_admin')
+    list_filter = ('is_admin',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_active', 'is_admin')}),
+        ('Permissions', {'fields': ('is_admin',)}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
         (None, {
-            'classes': ('wide'),
-            'fields': ('email', 'first_name', 'last_name', 'password1',
-                       'password2')
-        })
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2')}
+        ),
     )
-    search_fields = ('email'),
-    ordering = ('email'),
+    search_fields = ('email',)
+    ordering = ('email',)
     filter_horizontal = ()
 
 
