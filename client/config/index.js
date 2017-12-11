dbf6260a4c39be9c8303f8940f60c2a8a4272eb4
Matt Rosendin <matt.rosendin@gmail.com>
2017-12-11 08:21:24 -0800

    remove proxy to account uri

--- client/config/index.js
+++ client/config/index.js
@@ -12,14 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        pathRewrite: {
-          '^': ''
-        }
-      },
-      '/account': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8081',
         changeOrigin: true,
         pathRewrite: {
           '^': ''
