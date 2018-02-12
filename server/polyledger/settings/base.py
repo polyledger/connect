3a2283521736000495b014735918a86e81fa6476
Matt Rosendin <matt.rosendin@gmail.com>
2018-02-11 17:39:31 -0800

    Set correct template path

--- server/polyledger/settings/base.py
+++ server/polyledger/settings/base.py
@@ -68,7 +68,7 @@ REST_FRAMEWORK = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, '..', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
