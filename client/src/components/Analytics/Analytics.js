7699e8ff4929832fc82642919a6552bc537fde1e
Matt Rosendin <matt.rosendin@gmail.com>
2018-03-03 14:43:45 -0800

    Add Analytics component (no content yet)

---
+++ client/src/components/Analytics/Analytics.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Analytics.css";
+
+class Analytics extends Component {
+  render() {
+    return <div className="Analytics" />;
+  }
+}
+
+export default Analytics;
