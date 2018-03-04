7e41ed7010293193800bbf14a58350786d8e2983
Matt Rosendin <matt.rosendin@gmail.com>
2018-03-04 09:35:29 -0800

    Center iconav and add text

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
   render() {
     return (
       <div className="Navbar">
-        <ul className="nav nav-bordered navbar-padded justify-content-center">
+        <ul className="nav nav-bordered navbar-padded justify-content-center text-center">
           <li className="nav-item">
             <NavLink
               exact
@@ -16,6 +16,7 @@ class Navbar extends Component {
             >
               <i className="icon icon-pie-chart" />{" "}
               <span className="d-none d-sm-inline">Portfolio</span>
+              <small className="iconav-nav-label d-sm-none">Portfolio</small>
             </NavLink>
           </li>
           <li className="nav-item">
@@ -26,6 +27,7 @@ class Navbar extends Component {
             >
               <i className="icon icon-gauge" />{" "}
               <span className="d-none d-sm-inline">Analytics</span>
+              <small className="iconav-nav-label d-sm-none">Analytics</small>
             </NavLink>
           </li>
           <li className="nav-item">
@@ -36,6 +38,7 @@ class Navbar extends Component {
             >
               <i className="icon icon-wallet" />{" "}
               <span className="d-none d-sm-inline">Accounts</span>
+              <small className="iconav-nav-label d-sm-none">Accounts</small>
             </NavLink>
           </li>
           <li className="nav-item">
@@ -46,6 +49,7 @@ class Navbar extends Component {
             >
               <i className="icon icon-line-graph" />{" "}
               <span className="d-none d-sm-inline">Markets</span>
+              <small className="iconav-nav-label d-sm-none">Markets</small>
             </NavLink>
           </li>
           <li className="nav-item">
@@ -56,6 +60,7 @@ class Navbar extends Component {
             >
               <i className="icon icon-cog" />{" "}
               <span className="d-none d-sm-inline">Settings</span>
+              <small className="iconav-nav-label d-sm-none">Settings</small>
             </NavLink>
           </li>
         </ul>
