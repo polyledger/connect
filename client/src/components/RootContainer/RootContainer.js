2df8b8aa771b68ae08ab17d039720b5fb2f962b2
Matt Rosendin <matt.rosendin@gmail.com>
2018-03-16 14:36:37 -0700

    Provide store to app

--- client/src/components/RootContainer/RootContainer.js
+++ client/src/components/RootContainer/RootContainer.js
@@ -1,24 +1,31 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import rootReducer from "../../reducers";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import Header from "../Header/Header";
 import AppLayout from "../AppLayout/AppLayout";
 import EmptyLayout from "../EmptyLayout/EmptyLayout";
 import Footer from "../Footer/Footer";
 import "./RootContainer.css";
 
+let store = createStore(rootReducer);
+
 class RootContainer extends Component {
   render() {
     return (
-      <Router>
-        <div className="RootContainer">
-          <Header />
-          <Switch>
-            <Route path="/portfolio" component={AppLayout} />
-            <Route component={EmptyLayout} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="RootContainer">
+            <Header />
+            <Switch>
+              <Route path="/portfolio" component={AppLayout} />
+              <Route component={EmptyLayout} />
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
