88c05f37e0fa3d59633d47c1261afaec5db2098f
Matt Rosendin <matt.rosendin@gmail.com>
2018-04-06 17:41:11 -0700

    Work on tests

--- server/api/tests.py
+++ server/api/tests.py
@@ -1,22 +1,29 @@
 """
 Run this file with `python manage.py test api.tests`
+
+Testing in Django:
+https://docs.djangoproject.com/en/2.0/topics/testing/
+
+Testing with Celery:
+http://docs.celeryproject.org/en/latest/userguide/testing.html
 """
 
 from django.test import TestCase
+from django.test.utils import override_settings
 from django.contrib.auth import get_user_model
 from api.models import Portfolio, Coin, Position
 from api.tasks import fill_daily_historical_prices, allocate_for_user
 from api.trading import test_order
+from unittest.mock import patch
 
 
-class FillDailyHistoricalPricesTestCase(TestCase):
+class PriceTestCase(TestCase):
 
     def test_no_error(self):
         fill_daily_historical_prices()
 
 
-class AllocateForUserTestCase(TestCase):
-    # TODO: Write more unit tests
+class AllocationTestCase(TestCase):
 
     def setUp(self):
         Coin.objects.create(symbol='BTC', name='Bitcoin')
@@ -32,14 +39,19 @@ class AllocateForUserTestCase(TestCase):
             coin=Coin.objects.get(name='Bitcoin'),
             amount=10
         )
-        self.user.portfolio.save()
         self.user.save()
 
-    def test_no_error(self):
-        # symbols = ['BTC', 'ETH']
-        # risk_score = 1
-        # allocate_for_user(self.user.id, symbols, risk_score)
-        pass
+    @override_settings(
+        CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
+        CELERY_ALWAYS_EAGER=True,
+        BROKER_BACKEND='memory')
+    @patch('api.tasks.allocate_for_user')
+    def test_no_error(self, allocation):
+        pk = self.user.id
+        symbols = ['BTC', 'ETH']
+        risk_score = 1
+        allocation = allocate_for_user(pk, symbols, risk_score)
+        assert allocation is not None
 
 
 class TradingTestCase(TestCase):
@@ -47,3 +59,26 @@ class TradingTestCase(TestCase):
     def test_no_error(self):
         result = test_order()
         assert type(result) is dict
+
+
+class PortfolioTestCase(TestCase):
+
+    def setUp(self):
+        Coin.objects.create(symbol='BTC', name='Bitcoin')
+        self.user = get_user_model().objects.create_user(
+            first_name='Ari',
+            last_name='Hall',
+            email='ari@polyledger.com',
+            password='top_secret'
+        )
+        self.user.profile.risk_score = 3
+        Position.objects.create(
+            portfolio=self.user.portfolio,
+            coin=Coin.objects.get(name='Bitcoin'),
+            amount=10
+        )
+        self.user.save()
+
+    def test_fetch_portfolio(self):
+        portfolio = self.user.portfolio
+        assert isinstance(portfolio, Portfolio) is True
