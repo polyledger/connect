1007e91975ee0a1a0fd2006d80671dbc64b6e103
Matt Rosendin <matt.rosendin@gmail.com>
2018-04-21 12:50:08 -0700

    Save user's IP address

--- server/api/auth.py
+++ server/api/auth.py
@@ -2,7 +2,16 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from api.serializers import UserSerializer
-from api.models import User
+from api.models import User, IPAddress
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 
 class CustomAuthToken(ObtainAuthToken):
@@ -16,6 +25,10 @@ class CustomAuthToken(ObtainAuthToken):
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
         user = User.objects.get(email=user.email)
+
+        ip = get_client_ip(request)
+        ip_address, created = IPAddress.objects.get_or_create(ip=ip, user=user)
+        ip_address.save()
 
         return Response({
             'token': token.key,
