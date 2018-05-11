7171811aa2f246b03ab85d144976b77ed124583c
Matt Rosendin <matt.rosendin@gmail.com>
2018-05-11 15:54:24 -0700

    Fix for #28

--- server/polyledger/celery.py
+++ server/polyledger/celery.py
@@ -8,7 +8,8 @@ http://docs.celeryproject.org/en/latest/django/first-steps-with-django.html
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from celery.signals import worker_ready
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polyledger.settings.local')
@@ -25,14 +26,8 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-@worker_ready.connect
+# Fetch historical prices after API app completes migrations
+@receiver(post_migrate)
 def on_start(sender, **kwargs):
-    with sender.app.connection() as conn:
-        sender.app.send_task(
-            'api.tasks.fill_daily_historical_prices', connection=conn
-        )
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+    if sender.name == 'api':
+        app.send_task('api.tasks.fill_daily_historical_prices')
