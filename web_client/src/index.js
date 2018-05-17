7a9f0f70fca44a218eeb4746201167a1344e3831
Matt Rosendin <matt.rosendin@gmail.com>
2018-05-17 15:05:34 -0700

    Remove service worker

--- web_client/src/index.js
+++ web_client/src/index.js
@@ -5,7 +5,5 @@ import "font-awesome/css/font-awesome.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./components/Root/Root";
-import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<Root />, document.getElementById("root"));
-registerServiceWorker();
