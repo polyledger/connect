a68790e2789d5c8ad8199522c72cee7598e0efb9
Matt Rosendin <matt.rosendin@gmail.com>
2018-05-20 14:10:43 -0700

    Upgrade bootstrap package

--- web_client/src/index.js
+++ web_client/src/index.js
@@ -1,6 +1,6 @@
 import "babel-polyfill";
 import "@polyledger/bootstrap";
-import "@polyledger/bootstrap/css/index.css";
+import "@polyledger/bootstrap/css/dark.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
